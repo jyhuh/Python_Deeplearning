152edef9d03fa3c74499039bc7d8169a9a43abe5
Navillera <jyhuh35@gmail.com>
2019-10-26 01:20:25 +0900

    191026

--- Python_Study_win/NN_func.py
+++ Python_Study_win/NN_func.py
@@ -6,7 +6,7 @@ def step(x):
     return y.astype(np.int)
 
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1 / ( 1 + np.exp(-x) )
 
 def relu(x):
     return np.maximum(0,x)
--- Python_Study_win/wstudy_3.py
+++ Python_Study_win/wstudy_3.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pylab as plt
-import NN_func
+import pickle
+from NN_func import sigmoid, softmax
 #sigmoid
 #relu
 #identity
 #step
 #softmax
-
 # book97p
 import sys, os
 sys.path.append(os.pardir)
@@ -25,12 +25,47 @@ def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3  = network['W1'], network['W2'], network['W3']
+    b1, b2, b3  = network['b1'], network['b2'], network['b3']
+    a1 = np.dot( x, W1 ) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot( z1, W2 ) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot( z2, W3 ) + b3
+    y = softmax(a3)
+    return y
+
 img = x_train[0]
 label = t_train[0]
-print(label) #5
+print(label)
 
 print(img.shape)
 img = img.reshape(28,28)
 print(img.shape)
-print("28*28 =", 28*28)
+print("28*28 =", 28 * 28)
 img_show(img)
+
+
+x,t = get_data()
+
+network = init_network()
+
+accuracy_cnt = 0;
+
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy:" + str( float(accuracy_cnt) / len(x) ) )
